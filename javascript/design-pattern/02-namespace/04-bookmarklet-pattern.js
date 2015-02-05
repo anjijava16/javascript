ef0a7b4354439ccef3889cfc64158cbc9112d066
William Han <pwillhan@gmail.com>
2015-02-04 22:48:37 -0800

    add bookmarklet-pattern example

---
+++ javascript/design-pattern/02-namespace/04-bookmarklet-pattern.js
@@ -0,0 +1,4 @@
+//bookmarklet pattern has zero global footprint and are implemented as anonymous immediate functions
+(function(){
+  console.log('use anonymous immediate function for bookmarklet');
+})(this);
