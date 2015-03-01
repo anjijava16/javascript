71f8218e905f4122b9dd6f877c6e3794a6cf1a65
William Han <whan@godaddy.com>
2015-02-28 21:42:27 -0800

    move simulate long run into callback folder

--- nodejs/essentials/callback/simulate-long-run/simulate-long-run-test.js
+++ nodejs/essentials/callback/simulate-long-run/simulate-long-run-test.js
@@ -1,7 +1,8 @@
 console.log('#begin');
 function find(name, cb){
   if(name === 'Tom'){
-    // simulate long run
+    // simulate long run - get data from database
+    // return person object to callback person parameter
     setTimeout(function(){
       console.log('before invoke callback');
       cb(null, {name: 'Tom'});
