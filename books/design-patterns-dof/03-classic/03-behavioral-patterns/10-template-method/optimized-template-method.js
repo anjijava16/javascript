62dfbeff6d5238549a6a7bd9541c7349896288b3
William Han <whan@godaddy.com>
2015-03-08 22:25:57 -0700

    add template-method examples

---
+++ books/design-patterns-dof/03-classic/03-behavioral-patterns/10-template-method/optimized-template-method.js
@@ -0,0 +1,85 @@
+// log helper
+var log = (function () {
+  var log = "";
+  return {
+    add: function (msg) {
+      log += msg + "\n";
+    },
+    show: function () {
+      console.log(log);
+      log = "";
+    }
+  }
+})();
+
+var Patterns = {
+  namespace: function (name) {
+    var parts = name.split(".");
+    var ns = this;
+
+    for (var i = 0, len = parts.length; i < len; i++) {
+      ns[parts[i]] = ns[parts[i]] || {};
+      ns = ns[parts[i]];
+    }
+
+    return ns;
+  }
+};
+
+Patterns.namespace("Classic").Template = (function () {
+  var datastore = {
+    process: function () {
+      this.connect();
+      this.select();
+      this.disconnect();
+      return true;
+    }
+  };
+  return {datastore: datastore};
+})();
+
+Patterns.namespace("Utils").Common = (function () {
+  var inherit = function (proto) {
+    var F = function () {
+    };
+    F.prototype = proto;
+    return new F();
+  };
+  // log helper
+  var log = (function () {
+    var log = "";
+    return {
+      add: function (msg) {
+        log += msg + "\n";
+      },
+      show: function () {
+        console.log(log);
+        log = "";
+      }
+    }
+  })();
+  return {
+    inherit: inherit,
+    log: log
+  }
+})();
+
+(function run() {
+  var utils = Patterns.Utils.Common;
+  var store = Patterns.Classic.Template.datastore;
+  var mySql = utils.inherit(store);
+
+  // implement template steps
+  mySql.connect = function () {
+    utils.log.add("MySQL: connect step");
+  };
+  mySql.select = function () {
+    utils.log.add("MySQL: select step");
+  };
+  mySql.disconnect = function () {
+    utils.log.add("MySQL: disconnect step");
+  };
+
+  mySql.process();
+  utils.log.show();
+})();
---
+++ books/design-patterns-dof/README.md
@@ -0,0 +1,11 @@
+design-patterns-dof
+=====================
+
+## Good examples
+
+### 03-classic - 03-behavioral-patterns - 10-template-method
+
+```
+Template Methods are frequently used in general purpose frameworks or libraries that will be used by other developer.
+An example is an object that fires a sequences of events in response to an action, for example a process request.
+```
