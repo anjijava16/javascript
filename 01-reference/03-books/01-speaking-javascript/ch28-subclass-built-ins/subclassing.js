a7a75e11ff0e755b087c6ee4d1b16751634111a8
William Han <whan@godaddy.com>
2015-03-19 17:27:05 -0700

    update subclassing example

--- 01-reference/03-books/01-speaking-javascript/ch28-subclass-built-ins/subclassing.js
+++ 01-reference/03-books/01-speaking-javascript/ch28-subclass-built-ins/subclassing.js
@@ -1,11 +1,41 @@
+// ===============prototype==============
 function Super(x, y){
-  this.x = x;  // (1)
-  this.y = y;  // (1)
+  this.x = x;
+  this.y = y;
 }
-
 function Sub(x, y, z){
   // Add superproperties to subinstance
-  Super.call(this, x, y);  // (2)
+  Super.call(this, x, y);
   // Add subproperty
   this.z = z;
 }
+// ===============real world example==============
+// ---Base class---
+function Person(name, age){
+  this.name = name;
+  this.age = age;
+}
+Person.prototype = {
+  say: function(){
+    console.log('Hi from', this.name);
+  }
+}
+// ---Sub class---
+function Employee(name, age, salary){
+  // Add superproperties to subinstance
+  Person.call(this, name, age);
+  // Add subproperty
+  this.salary = salary;
+}
+// Copy super prototype to sub prototype
+Employee.prototype = Object.create(Person.prototype);
+
+// ---Usage---
+var employee = new Employee("Tom", 10, "$35,000");
+console.log(employee.name);
+console.log(employee.salary);
+employee.say();
+var person = new Person("Harry", 20);
+person.say();
+console.log(new Employee() instanceof Person);
+console.log(new Employee() instanceof Employee);
