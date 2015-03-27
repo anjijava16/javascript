6f687a3456749c4e7d3a3ceafd4a636154e94585
William Han <whan@godaddy.com>
2015-03-26 18:03:26 -0700

    add constructor example

--- 01-reference/03-books/01-speaking-javascript/ch17-objects-and-inheritance/01-Single-Objects/01-calling-functions/03-bind.js
+++ 01-reference/03-books/01-speaking-javascript/ch17-objects-and-inheritance/01-Single-Objects/01-calling-functions/03-bind.js
@@ -2,10 +2,19 @@ function func(){
   console.log('tihs: ' + this);
   console.log('arguments: ' + Array.prototype.slice.call(arguments));
 }
+console.log('func:', func);
 var bound = func.bind('abc', 1, 2);
+console.log('bound:', bound);
 bound(3);
 /*
  tihs: abc
+ arguments: 1,2,3
+ */
+var aryFunc = Function.prototype.bind.apply(func, ['xyz'].concat([1, 2]));
+console.log('nullAryFunc:', aryFunc);
+aryFunc(3);
+/*
+ tihs: xyz
  arguments: 1,2,3
  */
 
--- 01-reference/03-books/01-speaking-javascript/ch17-objects-and-inheritance/01-Single-Objects/02-constructors/01-apply.js
+++ 01-reference/03-books/01-speaking-javascript/ch17-objects-and-inheritance/01-Single-Objects/02-constructors/01-apply.js
@@ -2,8 +2,14 @@
   #Manually simulating an apply() for constructors
  */
 
+
+console.log('new Date(2011, 11, 24):', new Date(2011, 11, 24));
+console.log('Date:', Date);
 // Step1 - Pass the arguments to Date via a method call (they are not in array - yet)
 console.log(new (Date.bind(null, 2011, 11, 24)));  // Sat Dec 24 2011 00:00:00 GMT-0800 (PST)
+var nullAryFunc = Function.prototype.bind.apply(Date, [null].concat([2011, 11, 24]));
+console.log('nullAryFunc:', nullAryFunc);
+console.log('new nullAryFunc():', new nullAryFunc());
 
 // Step2 - Use apply() to hand an array to bind(). Because bind() is a method call, we can use apply()
 console.log(new (Function.prototype.bind.apply(Date, [null, 2011, 11, 24])));  // Sat Dec 24 2011 00:00:00 GMT-0800 (PST)
