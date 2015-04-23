b765ee7bd65defe232d631e4530a379a4f959a26
William Han <whan@godaddy.com>
2015-04-22 23:41:22 -0700

    update parser html example

--- nodejs/npm-modules/cheerio/parser-html.js
+++ nodejs/npm-modules/cheerio/parser-html.js
@@ -1,9 +1,24 @@
 var cheerio = require('cheerio'),
   $ = cheerio.load('<ul id="fruits"><li class="apple">Apple</li><li class="orange">Orange</li><li class="pear">Pear</li></ul>');
 
-console.log($('h2.title').innerHTML);
-//$('h2.title').text('Hello there!');
-$('h2').addClass('welcome');
+console.log('#Selectors');
+console.log('##$( selector, [context], [root] )');
+console.log($('.apple', '#fruits').text());
+console.log($('ul .pear').attr('class'));
+console.log($('li[class=orange]').html());
 
-var htmlElement = $.html();
-console.log(htmlElement);
+console.log('#Attributes');
+console.log('##.attr( name, value )');
+console.log($('ul').attr('id'));
+console.log($('.apple').attr('id', 'favorite').html());
+
+console.log('##.data( name, value )');
+console.log($('<div data-apple-color="red"></div>').data());
+console.log($('<div data-apple-color="red"></div>').data('appleColor'));
+var apple = $('.apple').data('kind', 'mac');
+console.log(apple.data('kind'));
+
+console.log('#Traversing');
+console.log('##.find(selector) .find(selection) .find(node)');
+console.log($('#fruits').find('li').length);
+console.log($('#fruits').find($('.apple')).length);
