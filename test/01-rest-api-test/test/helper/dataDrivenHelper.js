925e8778c3e45ebc955af2be42ebd17bd2486f9b
William Han <whan@godaddy.com>
2015-06-27 22:18:05 -0700

    delete dataDrivenHelper

--- test/01-rest-api-test/test/helper/dataDrivenHelper.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-var expect = require('chai').expect;
-var HttpClient = require('../common/httpClient').HttpClient;
-var config = require('../config/config.' + (process.env.NODE_ENV || 'development') + '.json');
-
